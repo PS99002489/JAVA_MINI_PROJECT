5c1830e79ac1d3ee5b0902ec2e97e4a1195b2750
PS99002489 <smita.senapati@ltts.com>
2020-11-11 10:46:49 +0530

    new commit

---
+++ src/test/java/Email/emailmain.java
@@ -0,0 +1,17 @@
+package Email;
+
+public class emailmain {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		email emp1=new email("Smita","Senapati");
+		 emp1.setalternateEmail("aditi.dutta@gmail.com");
+		 // System.out.println(emp1.getalternateEmail());
+		 
+		 emp1.changePassword("165grsgh!@#");
+		 System.out.println(emp1.getPassword());
+		 
+		 System.out.println( emp1.showdetails());
+	}
+
+}
